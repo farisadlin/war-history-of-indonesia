19b73260da11d8ef1215f9d340bd56e9fbb2de6b
farisadlin <farisnura@gmail.com>
2024-07-12 22:47:10 +0700

    chores: restructured folders name to be consistent to each other

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,10 +1,10 @@
 import type { MDXComponents } from "mdx/types";
 
 // This file is required to use MDX in `app` directory.
-export function useMDXComponents(_components: MDXComponents): MDXComponents {
+export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    // Allows customizing built-in _components, e.g. to add styling.
+    // Allows customizing built-in components, e.g. to add styling.
     // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
-    ..._components,
+    ...components,
   };
 }
